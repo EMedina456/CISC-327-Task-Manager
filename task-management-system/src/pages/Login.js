acb74a1fc047c2bd40ea179c8a0af97353752114
Eric <85941655+EMedina456@users.noreply.github.com>
2023-11-01 14:41:51 -0400

    Installed Firebase and created auth

--- task-management-system/src/pages/Login.js
+++ task-management-system/src/pages/Login.js
@@ -1,62 +1,67 @@
 // Program Intention: Handle the login of the user
-// Input/Output: Handle the username and password of the user
+// Input/Output: Handle the email and password of the user
 // Run Intention: Run with the entire website
 
 // Import files and dependencies here
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { auth } from '../config/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
 const Login = () => {
-  // Variables used in the page to handle the username and password
-  const [username, setUsername] = useState('');
+  // Variables used in the page to handle the email and password
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  // Handle the submit of the username and password, currently just console logs them
-  const handleSubmit = (e) => {
-    console.log('username', username);
-    console.log('password', password);
+  // Handle the submit of the email and password, currently just console logs them
+  const handleSubmit = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = '/';
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // Login Page
   return (
-    <div class="flex flex-col justify-center items-center h-screen w-screen">
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
       {/* Handle the form of login*/}
-      <h1 class="text-6xl text-left font-extrabold p-10">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div class="">
-          <label>
-            <h1 class="text-4xl font-bold mb-4">username</h1>
-            {/* Handle the username input*/}
-            <input
-              type="text"
-              id="username"
-              class="box-border h-8 w-44 p-4 border-4"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          {/* Handle the password input*/}
-          <label>
-            <h1 class="text-4xl font-bold mb-4 mt-4">password</h1>
-            <input
-              type="password"
-              id="password"
-              class="box-border h-8 w-44 p-4 border-4"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <div class="justify-center items-center text-center">
-            {/* Handle the submit button, which currently just returns to home page*/}
-            <Link to="/">
-              <p class="text-xl font-bold mt-2 underline decoration-[#0acdff]">
-                Submit
-              </p>
-            </Link>
-          </div>
+      <h1 className="text-6xl text-left font-extrabold p-10">Login</h1>
+      <div className="">
+        <label>
+          <h1 className="text-4xl font-bold mb-4">email</h1>
+          {/* Handle the email input*/}
+          <input
+            type="text"
+            id="email"
+            className="box-border h-8 w-44 p-4 border-4"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        {/* Handle the password input*/}
+        <label>
+          <h1 className="text-4xl font-bold mb-4 mt-4">password</h1>
+          <input
+            type="password"
+            id="password"
+            className="box-border h-8 w-44 p-4 border-4"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div className="justify-center items-center text-center">
+          {/* Handle the submit button, which currently just returns to home page*/}
+          <button
+            onClick={handleSubmit}
+            className="text-xl font-bold mt-2 underline decoration-[#0acdff]">
+            Submit
+          </button>
         </div>
-      </form>
-      <div class="justify-center items-center">
+      </div>
+      <div className="justify-center items-center">
         {/* Handle the sign up button, if user does not have an account, which currently just returns to sign up page*/}
         <Link to="/signup">
-          <p class="text-l font-bold mt-2 underline decoration-[#0acdff]">
+          <p className="text-l font-bold mt-2 underline decoration-[#0acdff]">
             Sign Up
           </p>
         </Link>
--- task-management-system/src/pages/Signup.js
+++ task-management-system/src/pages/Signup.js
@@ -1,19 +1,26 @@
 // Program Intention: Handle the signup of the user
-// Input/Output: Handle the username and password of the user
+// Input/Output: Handle the email and password of the user
 // Run Intention: Run with the entire website
 
 // Import files and dependencies here
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { auth } from '../config/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 const Signup = () => {
-  // Variables used in the page to handle the username and password
-  const [username, setUsername] = useState('');
+  // Variables used in the page to handle the email and password
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  // Handle the submit of the username and password, currently just console logs them
-  const handleSubmit = (e) => {
-    console.log('username', username);
-    console.log('password', password);
+  // Handle the submit of the email and password, currently just console logs them
+  const handleSubmit = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      window.location.href = '/';
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // Signup Page
@@ -21,38 +28,36 @@ const Signup = () => {
     <div class="flex flex-col justify-center items-center h-screen w-screen">
       {/* Handle the form of signup*/}
       <h1 class="text-6xl text-left font-extrabold p-10">Signup</h1>
-      <form onSubmit={handleSubmit}>
-        <div class="">
-          {/* Handle the username input*/}
-          <label>
-            <h1 class="text-4xl font-bold mb-4">username</h1>
-            <input
-              type="text"
-              id="username"
-              class="box-border h-8 w-44 p-4 border-4"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          {/* Handle the password input*/}
-          <label>
-            <h1 class="text-4xl font-bold mb-4 mt-4">password</h1>
-            <input
-              type="password"
-              id="password"
-              class="box-border h-8 w-44 p-4 border-4"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <div class="justify-center items-center text-center">
-            {/* Handle the submit button, which currently just returns to home page*/}
-            <Link to="/">
-              <p class="text-xl font-bold mt-2 underline decoration-[#0acdff]">
-                Submit
-              </p>
-            </Link>
-          </div>
+      <div class="">
+        {/* Handle the email input*/}
+        <label>
+          <h1 class="text-4xl font-bold mb-4">email</h1>
+          <input
+            type="text"
+            id="email"
+            class="box-border h-8 w-44 p-4 border-4"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        {/* Handle the password input*/}
+        <label>
+          <h1 class="text-4xl font-bold mb-4 mt-4">password</h1>
+          <input
+            type="password"
+            id="password"
+            class="box-border h-8 w-44 p-4 border-4"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div class="justify-center items-center text-center">
+          {/* Handle the submit button, which currently just returns to home page*/}
+          <button
+            onClick={handleSubmit}
+            class="text-xl font-bold mt-2 underline decoration-[#0acdff]">
+            Submit
+          </button>
         </div>
-      </form>
+      </div>
       <div class="justify-center items-center">
         {/* Handle the login button, if user already has an account, which currently just returns to login page*/}
         <Link to="/login">
